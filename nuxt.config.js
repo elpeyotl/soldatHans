cb2326dc12a6917acd594976c39b486aa0d7b1ed
Christopher Perez <christopher.perez@north-47.com>
2021-12-01 00:24:55 +0100

    added lazyload for images

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
-    ,
+    'nuxt-lazy-load',
   ],
 
   // Content module configuration: https://go.nuxtjs.dev/config-content

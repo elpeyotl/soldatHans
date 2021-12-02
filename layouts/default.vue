9aebd38f655b804957f211cab0172008b0783fbc
Christopher Perez <christopher.perez@north-47.com>
2021-12-02 10:21:24 +0100

    fixed header z-index

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
   >
     <div class="flex justify-center">
       <Header
-        class="w-full bg-white fixed lg:h-screen translate-x-1/2 left--1/2 lg:w-64 lg:left-16 lg:top-12 lg:mr-12"
+        class="w-full z-10 bg-white fixed lg:h-screen translate-x-1/2 left--1/2 lg:w-64 lg:left-16 lg:top-12 lg:mr-12"
       />
       <Nuxt class="mt-32 mb-64 w-full h-full lg:ml-80 lg:mt-40" />
       <AudioPlayer class="fixed bottom-0 w-64 lg:left-16 mb-6" />
